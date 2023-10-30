696661312bca21a3321301976cebd58e6a64db08
Dan Briant <daniel.briant@hotmail.com>
2023-10-30 21:31:19 +0100

    First calculator
    Pretty basic calculator but a start to the programming world I guess

---
+++ Calculator/main.js
@@ -0,0 +1,60 @@
+// const express = require('express')
+
+// calc = express()
+
+// calc.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+// calc.listen(3000, () => {
+//   console.log('listen')
+// });
+
+let equalPressed = 0;
+let buttonInput = document.querySelectorAll(".button");
+let input = document.getElementById("input");
+let allClear = document.getElementById("allClear");
+let equal = document.getElementById("equal-button");
+let erase = document.getElementById("delete");
+
+window.onLoad = () => {
+  input.value = "";
+};
+
+buttonInput.forEach((buttonClass) => {
+  buttonClass.addEventListener("click", () => {
+    if(equalPressed === 1) {
+      equalPressed = 0;
+    }
+    input.value += buttonClass.value;
+  });
+});
+
+equal.addEventListener("click", () =>{
+  equalPressed = 1;
+  let inputValue = input.value;
+  try {
+    let solution = eval(inputValue);
+    if (Number.isNaN(solution) || !Number.isFinite(solution)){
+      throw new Error("Invalid mathematical expression");
+  }
+    if (Number.isInteger(solution)){
+      input.value = solution;
+    } else {
+      input.value = solution.tofixed(2);
+    }
+  }
+  catch (error) {
+    alert(error.message);
+  }
+});
+
+allClear.addEventListener("click",  () =>{
+  input.value = ""
+} );
+
+erase.addEventListener ("click", () => {
+  input.value = input.value.substr(0, input.value.length - 1);
+});
+
+
